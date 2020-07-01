eba8a2d67f8a26efb3e1869ed286130476bd94e1
abrie <abrie@users.noreply.github.com>
2020-06-30 23:10:55 -0400

    Incorporate TailwindCSS. Note the extra step in package.json deploy task; where postcss is invoked to generate the css. This is due to a bug in the "build" step of snowpack, which skips css: https://github.com/pikapkg/snowpack/issues/549.

--- web/snowpack.config.js
+++ web/snowpack.config.js
@@ -1,44 +1,46 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   scripts: {
-    'mount:public': 'mount public --to /',
-    'mount:src': 'mount src --to /_dist_',
-    'run:tsc': 'tsc --noEmit',
-    'run:tsc::watch': '$1 --watch',
+    "mount:public": "mount public --to /",
+    "mount:src": "mount src --to /_dist_",
+    "mount:static": "mount static --to /",
+    "run:tsc": "tsc --noEmit",
+    "run:tsc::watch": "$1 --watch",
+    "build:css": "postcss",
   },
   plugins: [
     [
-      '@snowpack/plugin-webpack',
+      "@snowpack/plugin-webpack",
       {
         extendConfig: (config) => {
           const leafletImages = {
-            './images/layers.png$': path.resolve(
+            "./images/layers.png$": path.resolve(
               __dirname,
-              './node_modules/leaflet/dist/images/layers.png',
+              "./node_modules/leaflet/dist/images/layers.png"
             ),
-            './images/layers-2x.png$': path.resolve(
+            "./images/layers-2x.png$": path.resolve(
               __dirname,
-              './node_modules/leaflet/dist/images/layers-2x.png',
+              "./node_modules/leaflet/dist/images/layers-2x.png"
             ),
-            './images/marker-icon.png$': path.resolve(
+            "./images/marker-icon.png$": path.resolve(
               __dirname,
-              './node_modules/leaflet/dist/images/marker-icon.png',
+              "./node_modules/leaflet/dist/images/marker-icon.png"
             ),
-            './images/marker-icon-2x.png$': path.resolve(
+            "./images/marker-icon-2x.png$": path.resolve(
               __dirname,
-              './node_modules/leaflet/dist/images/marker-icon-2x.png',
+              "./node_modules/leaflet/dist/images/marker-icon-2x.png"
             ),
-            './images/marker-shadow.png$': path.resolve(
+            "./images/marker-shadow.png$": path.resolve(
               __dirname,
-              './node_modules/leaflet/dist/images/marker-shadow.png',
+              "./node_modules/leaflet/dist/images/marker-shadow.png"
             ),
           };
           Object.assign(config.resolve.alias, leafletImages);
           config.module.rules.push({
             test: /\.(png|svg|jpg|gif)$/,
             use: {
-              loader: 'file-loader',
+              loader: "file-loader",
             },
           });
           return config;
---
+++ web/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: {
+    content: ["./public/*.html"],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
