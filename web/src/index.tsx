8b59dcfb1ca3088717a9215faf8c88732b4412ed
abrie <abrie@users.noreply.github.com>
2020-07-03 13:25:43 -0400

    Refactor element creation into helpers.

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -14,54 +14,68 @@ async function searchAddress(address) {
     .then(({ candidates }) => candidates);
 }
 
-function getRecord(candidate) {
+async function getRecord(candidate) {
   return fetchCandidate(candidate)
     .then((resp) => checkResponse(resp))
     .then((resp) => resp.json());
 }
 
-function getRepresentative(district: number) {
+async function getRepresentative(district: number) {
   return fetchRepresentative(district)
     .then((resp) => checkResponse(resp))
     .then((resp) => resp.json());
 }
 
-function selectCandidate(candidate) {
-  getRecord(candidate).then(([record]) => {
-    const input = document.getElementById("address-input") as HTMLInputElement;
-    input.value = "";
-
-    const list = document.getElementById("candidates");
-    clearElement(list);
-
-    getRepresentative(record.COUNCIL_DIST)
-      .then((representative) => (
-        <div className="py-3 flex justify-between">
-          <div className="w-1/3 flex items-center justify-left">
-            <div className="text-xl">{representative.District}</div>
-          </div>
-          <div className="w-1/3 flex items-center justify-center">
-            <a className="text-xl" href={representative.Href}>
-              {representative.Name}
-            </a>
-          </div>
-          <div className="w-1/3 flex items-center justify-end">
-            <img
-              className="w-24"
-              src={`https://citycouncil.atlantaga.gov${representative.Image}`}
-            />
-          </div>
-        </div>
-      ))
-      .then((el) => {
-        const profile = document.getElementById("selected-candidate");
-        clearElement(profile);
-        profile.appendChild(el);
-      });
-  });
+function buildRepresentativeCard(representative) {
+  return (
+    <div className="py-3 flex justify-between">
+      <div className="w-1/3 flex items-center justify-left">
+        <div className="text-xl">{representative.District}</div>
+      </div>
+      <div className="w-1/3 flex items-center justify-center">
+        <a className="text-xl" href={representative.Href}>
+          {representative.Name}
+        </a>
+      </div>
+      <div className="w-1/3 flex items-center justify-end">
+        <img
+          className="w-24"
+          src={`https://citycouncil.atlantaga.gov${representative.Image}`}
+        />
+      </div>
+    </div>
+  );
 }
 
-function displayCandidates(candidates) {
+function clearAddressInput() {
+  const input = document.getElementById("address-input") as HTMLInputElement;
+  input.value = "";
+}
+
+function clearCandidateList() {
+  const list = document.getElementById("candidates");
+  clearElement(list);
+}
+
+function showRepresentativeCard(card) {
+  const profile = document.getElementById("selected-candidate");
+  clearElement(profile);
+  profile.appendChild(card);
+}
+
+async function selectCandidate(candidate) {
+  const [record] = await getRecord(candidate);
+
+  clearAddressInput();
+  clearCandidateList();
+
+  const representative = await getRepresentative(record.COUNCIL_DIST);
+  const card = buildRepresentativeCard(representative);
+
+  showRepresentativeCard(card);
+}
+
+function showCandidates(candidates) {
   const list = document.getElementById("candidates");
   clearElement(list);
 
@@ -89,7 +103,7 @@ function run() {
   addressInputElement.addEventListener("input", async (evt) => {
     const target = evt.currentTarget as HTMLInputElement;
     const candidates = await debouncedSearchAddress(target.value);
-    displayCandidates(candidates);
+    showCandidates(candidates);
   });
 }
 
